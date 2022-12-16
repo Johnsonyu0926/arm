ad00049b5e860e8246d1f71fa087d9722e2da15f
chengmengfei <chengmengfei@hzrisco.com>
2022-12-16 20:15:35 +0800

    修改流媒体播放逻辑

--- source/router/audioserver/mqtt_audioStreamStart.hpp
+++ source/router/audioserver/mqtt_audioStreamStart.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
+#include "volume.hpp"
 #include "json.hpp"
 #include "Singleton.hpp"
 #include <iostream>
 #include <thread>
-
 namespace asns {
     template<typename Quest, typename Result>
     class CReQuest;
-
+    template<typename T>
+    class CResult;
     class CAudioStreamStartResultData {
     public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CAudioStreamStartResultData, null)
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CAudioStreamStartResultData, null)
 
-        template<typename Quest, typename Result>
-        void do_success(const CReQuest<Quest, Result> &c) {}
+        template<typename Quest, typename Result,typename T>
+        void do_success(const CReQuest<Quest, Result> &c, CResult<T> &r) {}
 
     private:
         std::nullptr_t null;
@@ -22,31 +23,24 @@ namespace asns {
 
     class CAudioStreamStartData {
     public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CAudioStreamStartData, streamPath, roomId, priority, volume)
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CAudioStreamStartData, streamPath, roomId, priority, volume)
 
         int do_req() {
             std::cout << "volume:" << volume << std::endl;
             if (volume > 0) {
                 CVolumeSet volumeSet;
-                volumeSet.load();
-                int volumeTemp = volumeSet.getVolume();
-                volumeSet.setVolume(volume);
                 volumeSet.addj(volume);
-                volumeSet.saveToJson();
 
                 std::string streamUrl = streamPath + roomId;
                 char buf[256] = {0};
                 sprintf(buf,
-                        "ffmpeg -fflags nobuffer -r 30 -re -i %s -acodec mp3 -muxrate 7000000 -b:a 48k -max_delay 100 -bitrate 7000000 -fifo_size 27887 -burst_bits 1000000 -g 10 -b 7000000 -analyzeduration 10000000 -f mp3 - | madplay - &",
+                        "ffmpeg -fflags nobuffer -r 30 -re -i %s -acodec mp3 -muxrate 7000000 -b:a 48k -max_delay 100 -bitrate 7000000 -fifo_size 27887 -burst_bits 1000000 -g 10 -b 7000000 -analyzeduration 10000000 -f mp3 - | madplay -",
                         streamUrl.c_str());
                 std::cout << "system:" << buf << std::endl;
                 std::thread([&] {
                     Singleton::getInstance().setStatus(1);
                     system(buf);
                     Singleton::getInstance().setStatus(0);
-                    volumeSet.setVolume(volumeTemp);
-                    volumeSet.addj(volume);
-                    volumeSet.saveToJson();
                 }).detach();
 
             } else {
@@ -61,8 +55,7 @@ namespace asns {
                     Singleton::getInstance().setStatus(1);
                     system(buf);
                     Singleton::getInstance().setStatus(0);
-                }).detach();
-
+                }).join();
             }
             return 1;
         }
