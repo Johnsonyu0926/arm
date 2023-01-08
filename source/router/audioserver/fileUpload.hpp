f4d3ea27429eeb69b8cdf44b1956e96032d3fab9
chengmengfei <chengmengfei@hzrisco.com>
2023-01-08 18:53:59 +0800

    修改文件上传逻辑

--- source/router/audioserver/fileUpload.hpp
+++ source/router/audioserver/fileUpload.hpp
@@ -1,20 +1,24 @@
 #pragma once
 
 #include "json.hpp"
+#include "utils.h"
 #include "add_custom_audio_file.hpp"
 
-namespace asns
-{
-    class CFileUploadResult
-    {
+namespace asns {
+    class CFileUploadResult {
     public:
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(CFileUploadResult, cmd, resultId, msg)
 
-        int do_success()
-        {
+        int do_success() {
             cmd = "FileUpload";
             resultId = 1;
-            msg = "音频文件上传成功";
+            msg = "file upload success";
+        }
+
+        int do_fail(std::string str) {
+            cmd = "FileUpload";
+            resultId = 2;
+            msg = str;
         }
 
     private:
@@ -23,28 +27,37 @@ namespace asns
         std::string msg;
     };
 
-    class CFileUpload
-    {
+    class CFileUpload {
     public:
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(CFileUpload, downloadUrl, fileName)
 
-        int do_req(CSocket *pClient)
-        {
-            CAddCustomAudioFileData node;
-            node.filePath = downloadUrl;
-            strncpy(node.filename, fileName.c_str(), fileName.length());
-
-            CAudioCfgBusiness audioCfgBusiness;
-            audioCfgBusiness.load();
-
-            CAddCustomAudioFileBusiness addCustomAudioFileBusiness;
-            addCustomAudioFileBusiness.savePrefix = audioCfgBusiness.getAudioFilePath();
-            addCustomAudioFileBusiness.append(node);
-
+        int do_req(CSocket *pClient) {
             CFileUploadResult fileUploadResult;
-            fileUploadResult.do_success();
+            CUtils utils;
+            CAudioCfgBusiness cfg;
+            std::string res = utils.get_upload_result(downloadUrl.c_str(), cfg.getAudioFilePath().c_str(),
+                                                      fileName.c_str());
+            std::cout << "res:-----" << res << std::endl;
+            if (res.empty()) {
+                fileUploadResult.do_fail("fail");
+            } else if (res.find("error") != std::string::npos) {
+                fileUploadResult.do_fail("Connection unreachable");
+            } else {
+                CAddCustomAudioFileData node;
+                node.filePath = downloadUrl;
+                node.customAudioName = fileName;
+                CAddCustomAudioFileBusiness business;
+                if (!business.exist(fileName)) {
+                    business.business.push_back(node);
+                    business.saveToJson();
+                    fileUploadResult.do_success();
+                } else {
+                    fileUploadResult.do_success();
+                }
+            }
             json js = fileUploadResult;
             std::string str = js.dump();
+            std::cout << "return json: " << str << std::endl;
             pClient->Send(str.c_str(), str.length());
         }
 
