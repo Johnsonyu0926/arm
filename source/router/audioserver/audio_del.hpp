6c2b60d291d456d615cfc8234669859e6d02f034
chengmengfei <chengmengfei@hzrisco.com>
2023-01-08 18:54:57 +0800

    修改删除音频逻辑

--- source/router/audioserver/audio_del.hpp
+++ source/router/audioserver/audio_del.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "json.hpp"
 #include "add_custom_audio_file.hpp"
 #include "add_column_custom_audio_file.hpp"
@@ -6,31 +7,27 @@
 #include "utils.h"
 #include "getaudiolist.hpp"
 //{"cmd":"AudioDelete","deleteName":"23.mp3","storageType":1}
-namespace asns
-{
+namespace asns {
 
-    class CDeleteAudioResult
-    {
+    class CDeleteAudioResult {
     private:
         string cmd;
         int resultId;
         string msg;
-        vector<CGetAudioData> data;
+        vector <CGetAudioData> data;
 
     public:
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(CDeleteAudioResult, cmd, resultId, data, msg)
 
     public:
-        int do_success()
-        {
+        int do_success() {
             cmd = "AudioDelete";
             resultId = 1;
             msg = "AudioDelete handle success";
 
             CAddCustomAudioFileBusiness audios;
             audios.load();
-            for (int i = 0; i < audios.business.size(); i++)
-            {
+            for (int i = 0; i < audios.business.size(); i++) {
                 CGetAudioData v;
                 v.storageType = 0;
                 v.type = 32;
@@ -45,53 +42,19 @@ namespace asns
             }
         }
     };
-    class CDeleteAudio
-    {
+
+    class CDeleteAudio {
     public:
         NLOHMANN_DEFINE_TYPE_INTRUSIVE(CDeleteAudio, cmd, deleteName, storageType)
 
-        int do_del(std::string &name, int type)
-        {
-
+        int do_del(std::string &name, int type) {
             CAddCustomAudioFileBusiness audios;
-            audios.load();
-            for (auto it = audios.business.begin(); it != audios.business.end(); ++it)
-            {
-                if (it->customAudioName == name)
-                {
-                    char cmd[256];
-                    CAudioCfgBusiness cfg;
-                    cfg.load();
-
-                    sprintf(cmd, "rm %s%s", cfg.getAudioFilePath().c_str(), name.c_str());
-                    std::cout << cmd << std::endl;
-                    system(cmd);
-                    audios.business.erase(it);
-                    audios.saveToJson();
-                    return 1;
-                }
-            }
-            CAddColumnCustomAudioFileBusiness bussines;
-            bussines.Columnload();
-            for (auto it = bussines.business.begin(); it != bussines.business.end(); ++it)
-            {
-                if (it->getName() == name)
-                {
-                    char cmd[256];
-                    CAudioCfgBusiness cfg;
-                    cfg.load();
-
-                    sprintf(cmd, "rm %s%s", cfg.getAudioFilePath().c_str(), name.c_str());
-                    system(cmd);
-                    bussines.business.erase(it);
-                    bussines.saveJson();
-                    return 1;
-                }
-            }
+            CAddColumnCustomAudioFileBusiness business;
+            if (audios.deleteAudio(name) || business.deleteAudio(name))return 1;
             return 0;
         }
-        int do_req(CSocket *pClient)
-        {
+
+        int do_req(CSocket *pClient) {
             do_del(deleteName, storageType);
             CDeleteAudioResult res;
             res.do_success();
