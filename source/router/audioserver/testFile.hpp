4384c495cf89e87afa0bd0b14fe5a5a7066a7499
chengmengfei <chengmengfei@hzrisco.com>
2023-01-13 09:15:24 +0800

    ffmpegCmd修改为单独配置文件

---
+++ source/router/audioserver/testFile.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "json.hpp"
+#include "audiocfg.hpp"
+
+namespace asns {
+    class TestFile {
+    public:
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(TestFile, ffmpegCmd)
+
+    public:
+        std::string ffmpegCmd;
+    };
+
+    class TestFileBusiness {
+    public:
+        const std::string TEST_FILE = "/cfg/test.json";
+
+        TestFileBusiness() {
+            CAudioCfgBusiness business;
+            business.load();
+            filePath = business.business[0].savePrefix + TEST_FILE;
+            std::ifstream ifs(filePath);
+            if (!ifs.is_open()) {
+                std::cout << "open test.json fail" << std::endl;
+            }
+            json js;
+            ifs >> js;
+            testFile = js;
+        }
+
+        void load() {
+            std::ifstream ifs(filePath);
+            if (!ifs.is_open()) {
+                std::cout << "open test.json fail" << std::endl;
+                return;
+            }
+            json js;
+            ifs >> js;
+            testFile = js;
+        }
+
+        std::string getFfmpegCmd() const {
+            return testFile.ffmpegCmd;
+        }
+
+    private:
+        TestFile testFile;
+        std::string filePath;
+    };
+}
