586241efdc43dd17ce04fc44aceb2b4c0255f1c9
chengmengfei <chengmengfei@hzrisco.com>
2023-01-15 19:46:53 +0800

    修改设置音量为dac0gan

--- source/router/audioserver/volume.hpp
+++ source/router/audioserver/volume.hpp
@@ -41,7 +41,7 @@ namespace asns {
 
     private:
         int vo[8] = {0, 100, 105, 110, 115, 120, 125, 130};
-        int vo_ros[8] = {0, 20, 25, 30, 35, 40, 45, 50};
+        int vo_ros[8] = {0, 16, 18, 20, 26, 28, 30, 32};
         std::string filePath;
     public:
         CVolumeSet() : volume(3) {
@@ -65,7 +65,7 @@ namespace asns {
             char cmd[128];
             CUtils utils;
             if (utils.is_ros_platform()) {
-                sprintf(cmd, "amixer set PCM playback %d", vo_ros[v]);
+                sprintf(cmd, "dspset /dev/ttyS2 dac0gan %d", vo_ros[v]);
             } else {
                 sprintf(cmd, "amixer set Headphone Playback %d", vo[v]);
             }
@@ -78,6 +78,9 @@ namespace asns {
             j["volume"] = volume;
             std::ofstream o(filePath);
             o << std::setw(4) << j << std::endl;
+            char buf [64] = {0};
+            sprintf(buf,"cm set_val sys dac0gan %d",vo_ros[volume]);
+            system(buf);
             return 0;
         }
 
