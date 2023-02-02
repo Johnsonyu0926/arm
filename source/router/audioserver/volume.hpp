ad83314249d0d27b0dcad589743542150a352b88
chengmengfei <chengmengfei@hzrisco.com>
2023-02-02 10:55:51 +0800

    bug fix

--- source/router/audioserver/volume.hpp
+++ source/router/audioserver/volume.hpp
@@ -41,7 +41,8 @@ namespace asns {
 
     private:
         int vo[8] = {0, 100, 105, 110, 115, 120, 125, 130};
-        int vo_ros[8] = {0, 16, 18, 20, 26, 28, 30, 32};
+        int vo_ros[8] = {0, 10, 20, 25, 30, 40, 45, 50};
+        //int vo_ros[8] = {0, 16, 18, 20, 26, 28, 30, 32};
         std::string filePath;
     public:
         CVolumeSet() : volume(3) {
@@ -65,7 +66,8 @@ namespace asns {
             char cmd[128];
             CUtils utils;
             if (utils.is_ros_platform()) {
-                sprintf(cmd, "dspset /dev/ttyS2 dac0gan %d", vo_ros[v]);
+                sprintf(cmd, "amixer sset PCM %d", vo_ros[v]);
+                //sprintf(cmd, "dspset /dev/ttyS2 dac0gan %d", vo_ros[v]);
             } else {
                 sprintf(cmd, "amixer set Headphone Playback %d", vo[v]);
             }
@@ -78,8 +80,8 @@ namespace asns {
             j["volume"] = volume;
             std::ofstream o(filePath);
             o << std::setw(4) << j << std::endl;
-            char buf [64] = {0};
-            sprintf(buf,"cm set_val sys dac0gan %d",vo_ros[volume]);
+            char buf[64] = {0};
+            sprintf(buf, "cm set_val sys dac0gan %d", vo_ros[volume]);
             system(buf);
             return 0;
         }
