274f1dc5bbe8ed142dac6fc6a63c6e47a512340e
chengmengfei <chengmengfei@hzrisco.com>
2023-03-16 11:28:05 +0800

    add gpio sleep

--- source/router/audioserver/utils.h
+++ source/router/audioserver/utils.h
@@ -1024,6 +1024,7 @@ public:
                                     CGpio::getInstance().set_gpio_off();
                                 }
                             }
+                            sleep(1);
                         }
                     });
                 }
